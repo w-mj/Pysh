92bb30088fe620449d7c622d081658281f6fa6b1
w-mj <wmj@alphamj.cn>
2020-11-17 14:34:25 +0800

    Exec as subclass of Filter

--- pysh/lib/exec.py
+++ pysh/lib/exec.py
@@ -36,59 +36,60 @@ class ResultTable:
         self._input = input
 
 
-class Exec:
+class Exec(Filter):
     class State(enum.Enum):
         NOT_START = 0
         RUNNING = 1
         FINISHED = 2
         NOT_RUN = 3
 
-    def __init__(self, cmd: str, input=None, capture_output=False, pre: List['Exec'] = []):
+    def __init__(self, cmd: str, upstream=None, capture_output=False, pre: List['Exec'] = []):
+        super().__init__(Filter.PIPE, upstream)
         self._cmd = cmd
         self._state = Exec.State.NOT_START
         self._result = None
         self._process = None
-        self._input = input
+        self._upstream = upstream
         self._capture_output = capture_output
         self._pre = pre
 
     def __exec_func(self):
         self._state = Exec.State.RUNNING
-        if self._input is None:
-            input_str = self._input
-        elif isinstance(self._input, Filter):
-            if self._input.type in (Filter.PIPE, Filter.FUNC, Filter.FILTER):
-                if self._input.returncode() != 0:
-                    log.debug(f"pipe upstream ({self._input}) fail")
+        if self._upstream is None:
+            input_str = self._upstream
+        elif isinstance(self._upstream, Filter):
+            if self._upstream.type in (Filter.PIPE, Filter.FUNC, Filter.FILTER):
+                if self._upstream.returncode() != 0:
+                    log.debug(f"pipe upstream ({self._upstream}) fail")
                     self._state = self.State.NOT_RUN
-                    self._result = self._input.result()
+                    self._result = self._upstream.result()
                     return None
-                input_str = self._input.stdout()
-            elif self._input.type == Filter.IF_SUCCESS:
-                if self._input.upstream.returncode() != 0:
-                    log.debug(f"logic and upstream ({self._input}) fail")
+                input_str = self._upstream.stdout()
+            elif self._upstream.type == Filter.IF_SUCCESS:
+                if self._upstream._upstream.returncode() != 0:
+                    log.debug(f"logic and upstream ({self._upstream}) fail")
                     self._state = self.State.NOT_RUN
                     # self._result = subprocess.CompletedProcess(self._cmd, self._input.upstream.returncode())
-                    self._result = self._input.result()
+                    self._result = self._upstream.result()
                     return None
                 input_str = None
-            elif self._input.type == Filter.IF_FAIL:
-                if self._input.upstream.returncode() == 0:
-                    log.debug(f"logic or upstream ({self._input}) success")
+            elif self._upstream.type == Filter.IF_FAIL:
+                if self._upstream._upstream.returncode() == 0:
+                    log.debug(f"logic or upstream ({self._upstream}) success")
                     self._state = self.State.NOT_RUN
                     # self._result = subprocess.CompletedProcess(self._cmd, self._input.upstream.returncode())
-                    self._result = self._input.result()
+                    self._result = self._upstream.result()
                     return None
                 input_str = None
             else:
                 input_str = None
-        elif isinstance(self._input, (str, bytes)):
-            input_str = self._input
+        elif isinstance(self._upstream, (str, bytes)):
+            input_str = self._upstream
         else:
-            raise Exception(f"type {type(self._input)} is not supported as input")
+            raise Exception(f"type {type(self._upstream)} is not supported as input")
         log.debug("start run " + self._cmd)
         log.debug("with input " + str(input_str))
-        self._cmd = self._parse_exec_cmd(self._cmd, self._input)
+        self._cmd = self._parse_exec_cmd(self._cmd, self._upstream)
         self._process = subprocess.Popen(self._cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if isinstance(input_str, str):
             input_str = input_str.encode()
@@ -144,17 +145,27 @@ class Exec:
 
     @property
     def input(self):
-        return self._input
-
-    @input.setter
-    def input(self, value):
-        self._input = value
+        return self._upstream
 
     def __or__(self, other):
-        if isinstance(other, Exec):
+        return self.set_input(other)
+
+    def __ror__(self, other):
+        if isinstance(other, (str, Exec)):
+            self._upstream = other
+        return self
+
+    def run_if_fail(self, ano: 'Exec'):
+        self._upstream = Filter(Filter.IF_FAIL, ano)
+
+    def run_if_success(self, ano: 'Exec'):
+        self._upstream = Filter(Filter.IF_SUCCESS, ano)
+
+    def set_input(self, other):
+        if isinstance(other, Filter):
             # 管道传递
             # other._input = self
-            other._input = Filter(Filter.PIPE, self)
+            other._upstream = Filter(Filter.PIPE, self)
             return other
         elif isinstance(other, str):
             # 正则过滤
@@ -163,23 +174,13 @@ class Exec:
                 res = pat.finditer(result.stdout)
                 return '\n'.join(map(lambda x: x if isinstance(x, str) else ' '.join(x), res))
 
-            other._input = Filter(Filter.FUNC, self, _filter)
+            other = FuncFilter(_filter, self)
         elif callable(other):
-            other._input = Filter(Filter.FUNC, self, other)
+            other = FuncFilter(other, self)
+        else:
+            raise RuntimeError("Unsupported type " + other)
 
-    def __ror__(self, other):
-        if isinstance(other, (str, Exec)):
-            self._input = other
-        return self
-
-    def run_if_fail(self, ano: 'Exec'):
-        self._input = Filter(Filter.IF_FAIL, ano)
-
-    def run_if_success(self, ano: 'Exec'):
-        self._input = Filter(Filter.IF_SUCCESS, ano)
-
-    def set_input(self, ano):
-        self._input = ano
+        return other
 
     def _parse_exec_cmd(self, cmd, input: Union[Filter, 'Exec']):
         arr = ResultTable(input)
--- pysh/lib/filter.py
+++ pysh/lib/filter.py
@@ -10,34 +10,25 @@ class Filter:
     PIPE = 4
     FILTER = 5
 
-    def __init__(self, type, upstream, func: Callable[[Optional[subprocess.CompletedProcess]], str] = None):
-        self.upstream = upstream
-        self.func = func
+    def __init__(self, type, upstream: Optional['Filter']):
+        self._upstream = upstream
         self.type = type
         self._stdout = None
-
-    def __call__(self):
-        if self.upstream:
-            return self.func(self.upstream)
-        return self.func(None)
 
     def stdout(self):
         if self._stdout:
             return self._stdout
-        if self.type in (self.FUNC, self.FILTER):
-            self._stdout = self()
-        else:
-            self._stdout = self.upstream.stdout()
+        self._stdout = self._upstream.stdout()
         return self._stdout
 
     def returncode(self):
-        return self.upstream.returncode()
+        return self._upstream.returncode()
 
     def set_input(self, ano):
-        self.upstream = ano
+        self._upstream = ano
 
     def result(self):
-        return self.upstream.result()
+        return self._upstream.result()
 
     def __str__(self):
         return str(self.result())
@@ -46,21 +37,26 @@ class Filter:
         return str(self)
 
 
-class RegexFilter(Filter):
-    def __init__(self, reg):
-        super().__init__(Filter.FUNC, None)
+class FuncFilter(Filter):
+    def __init__(self, func: Callable[[Optional[subprocess.CompletedProcess]], str] = None, upstream: Optional['Filter']=None):
+        super().__init__(Filter.FUNC, upstream)
+        self._func = func
+        self._stdout = None
 
-        def _filter(result: subprocess.CompletedProcess):
+    def stdout(self):
+        if not self._stdout:
+            self._stdout = self._func(self._upstream)
+        return self._stdout
+
+
+class RegexFilter(FuncFilter):
+    def __init__(self, reg):
+        super().__init__(None)
+
+        def _filter(result: Optional[subprocess.CompletedProcess]):
             if result is None:
                 return reg
             pat = re.compile(reg)
             res = pat.findall(result.stdout().decode())
             return '\n'.join(map(lambda x: x if isinstance(x, str) else ' '.join(x), res))
-
-        self.func = _filter
-
-
-class FuncFilter(Filter):
-    def __init__(self, func):
-        super().__init__(Filter.FUNC, None, func)
-
+        self._func = _filter
